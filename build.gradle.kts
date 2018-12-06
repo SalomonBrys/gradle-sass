7b3e3f101a8160f288e55d8214274770f088d68b
Salomon BRYS <salomon.brys@gmail.com>
2018-12-06 11:30:09 +0100

    Version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,21 @@
-import com.jfrog.bintray.gradle.BintrayExtension
-
 plugins {
     id("kotlin-platform-jvm")
     `kotlin-dsl`
-    id("maven-publish")
+    `maven-publish`
     id("com.gradle.plugin-publish") version "0.9.10"
-    id("com.jfrog.bintray") version "1.8.0"
 }
 
 group = "com.github.salomonbrys.gradle.sass"
-version = "1.0.0"
+version = "1.0.1"
 description = "A Gradle plugin to download & run the official dart-sass release with Gradle"
 
 repositories {
     jcenter()
     google()
     maven(url = "https://plugins.gradle.org/m2/")
-    maven(url = "https://dl.bintray.com/kotlin/kotlin-eap")
 }
 
-val kotlinVersion = "1.3.0-rc-146"
+val kotlinVersion = "1.3.10"
 
 dependencies {
     implementation(gradleApi())
